77a7c87d40a45a98897db2956c78744b4c84db1d
Marc Vouve <mvouve@gmail.com>
2015-01-31 14:39:13 -0800

    Cleaning up mistakes with GIT - added _template

---
+++ application/views/_template.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <Title>{name}</Title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/css/custom.css"
+    </head>
+    <body>
+        <div id="header" class="container-fluid">
+            <img>
+            <h1>{Name}</h1>
+            {menubar}
+        </div>
+        <div class="container">
+        {content}
+        </div>
+        <footer class="container-fluid">{name}{year}{email}</footer>
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <script src="/assets/js/banner.js"></script>
+    </body>
+</html>
