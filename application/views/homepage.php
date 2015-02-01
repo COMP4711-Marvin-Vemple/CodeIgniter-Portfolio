0437e1bdf7d761f926b3b8920cc41a07c45949e9
Marc Vouve <mvouve@gmail.com>
2015-01-31 20:20:43 -0800

    Got carousel working as intended

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -10,36 +10,44 @@ Cool banner thing goes here
 <!--
 Gallery Goes here
 -->
-<div id="gallery" class="carousel slide" data-ride="carousel">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
-    <li data-target="#gallery" data-slide-to="0"></li>
-    <li data-target="#gallery" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
-    
-    {featured_projects}
-    <div class="itemx">
-    <img src="..." alt="...">
+    {first_featured_project}
+      <div class="item active">
+      <a href="/project/id/{id}"><img src={image} alt="...">
         <div class="carousel-caption">
             <h3>{title}</h3>
-            <p>...</p>
-        </div>
-    </div>
+            <p>{description}</p>
+        </div></a>
+      </div>
+    {/first_featured_project}
+    {featured_projects}
+      <div class="item">
+        <a href="/project/id/{id}"><img src={image} alt="...">
+        <div class="carousel-caption">
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </div></a>
+      </div>
     {/featured_projects}
-  </div>
-  
     
+  </div>
 
-  <!-- Controls -->
-  <a class="left carousel-control" href="#gallery" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="false"></span>
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="right carousel-control" href="#gallery" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="false"></span>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
 </div>
