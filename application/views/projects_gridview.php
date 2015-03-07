b5ce0f7d85ec2d2a0df970e31666bc8dff44a3d0
Calvin Rempel <rempel.calvin@gmail.com>
2015-03-06 19:54:55 -0800

    -Added project filtering toolbar
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- application/views/projects_gridview.php
+++ application/views/projects_gridview.php
@@ -1,5 +1,11 @@
-<div class="btn-group" role="group" aria-label="..." id="mode-toggle">
-    <a type="button" class="btn btn-default active"><span class="glyphicon glyphicon-th"></span></a>
-  <a type="button" class="btn btn-default" href="/project/listview/1/"><span class="glyphicon glyphicon-th-list"></span></a>
+<div class="btn-toolbar">
+    
+    <!-- VIEW SELECTION -->
+    <div class="btn-group project-toolbar" role="group">
+        <a type="button" class="btn btn-default mode-select active"><span class="glyphicon glyphicon-th"></span></a>
+        <a type="button" class="btn btn-default mode-select" href="/project/listview/{page}/{sort}/{sortOrder}/{filter}"><span class="glyphicon glyphicon-th-list"></span></a>
+    </div>
+
+    {toolbar}
 </div>
 {projects}
