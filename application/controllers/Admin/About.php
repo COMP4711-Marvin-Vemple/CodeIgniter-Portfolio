bb291ac7764c1064c264393c551b685a121a0680
Calvin Rempel <rempel.calvin@gmail.com>
2015-03-07 15:53:48 -0800

    -Added TinyMCE output to About controller. -Added input saving to file.
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- application/controllers/Admin/About.php
+++ application/controllers/Admin/About.php
@@ -7,17 +7,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author Calvin Rempel
  */
 class About extends Application {
+    
+    private static $ABOUT_FILE   = 'application/views/about.php';
 
     /**
      * Controller for "Admin/About" page.
      */
     public function index() {
+        // Check if the form was submitted and if so, save the data
+        $this->saveData();
+        
+        // Setup the rendering configuration
         $this->data['mode'] = 'admin';
         $this->data['pagebody'] = 'admin/about';
         
         // Load current contents of about page into template var 'about'
-        $this->data['about'] = file_get_contents('application\views\about.php');
+        $this->data['about'] = file_get_contents(About::$ABOUT_FILE);
+        
+        // Load TinyMCE
+        $this->data['scripts'][] = array('script'=>"//tinymce.cachefly.net/4.1/tinymce.min.js");
+        $this->data['components'][] = array('component'=>$this->parser->parse('components/tinymce', array('selector'=>'.editor')));
         
         $this->render();
+    }
+    
+    /**
+     * Save the Submitted data if the form was submitted.
+     */
+    private function saveData() {
+        $newData = '';
+        
+        // If the data was submitted, write it to the file
+        if (($newData = $this->input->post('about', true)) != false) {
+            file_put_contents(About::$ABOUT_FILE, $newData);
+        }
     }
 }
