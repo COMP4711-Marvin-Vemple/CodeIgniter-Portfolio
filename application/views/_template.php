3923147a297a2203ddb9f60fd01e49acff9f546f
Calvin Rempel <rempel.calvin@gmail.com>
2015-03-07 14:39:14 -0800

    -Replaced caboose tags with custom script/style tags
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -6,6 +6,8 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="/assets/css/custom.css"
         {styles}
+            <link rel="stylesheet" href="{style}"/>
+        {/styles}
     </head>
     <body>
         <div id="header" class="container-fluid">
@@ -21,5 +23,7 @@
         <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
         {scripts}
+            <script src="{script}"></script>
+        {/scripts}
     </body>
 </html>
