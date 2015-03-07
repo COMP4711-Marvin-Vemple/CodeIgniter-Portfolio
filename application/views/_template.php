431f04f925432fc98787ef85de800da71ae24f5d
Calvin Rempel <rempel.calvin@gmail.com>
2015-03-07 14:34:06 -0800

    - Removed Caboose Tags
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="/assets/css/custom.css"
-        {caboose_styles}
+        {styles}
     </head>
     <body>
         <div id="header" class="container-fluid">
@@ -20,7 +20,6 @@
         <footer class="container-fluid">{name} {year} {email}</footer>
         <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-        {caboose_scripts}
-        {caboose_trailings}
+        {scripts}
     </body>
 </html>
