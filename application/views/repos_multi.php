dbdc6d07b174bdf7c23561fca454867ba878cec7
Calvin Rempel <rempel.calvin@gmail.com>
2015-03-07 20:59:20 -0800

    -Updated Repository views to conform to new template variables. -Made external links open in new tabs -Changed language -Changed URL of single repo view
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- application/views/repos_multi.php
+++ application/views/repos_multi.php
@@ -2,10 +2,9 @@
 <table class="table">
     {repos}
     <tr>
-        <td>{title}</td>
+        <td><a href="{url}" target="_blank" title="View on GitHub">{title}</a></td>
         <td>{description}</td>
-        <td><a class="btn btn-primary pull-right" href="/repository/id/{id}">View Repo</a>
+        <td><a class="btn btn-primary pull-right" href="/repository/commits/{title}" title="View Commits">View Recent Commits</a>
     </tr>
     {/repos}
 </table>
-{pagination}
