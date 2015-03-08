5a811376a523f9c3eaf18313f995a02c8942d4a4
Marc Vouve <mvouve@gmail.com>
2015-03-07 23:47:07 -0800

    Basework for image upload

--- application/views/admin/post-edit.php
+++ application/views/admin/post-edit.php
@@ -1,7 +1,18 @@
 <form action="/admin/post" method="POST">
-    <textarea name="post" class="editor">
-        {post}
-    </textarea>
+    <div class="input-group">
+        <label for="id">ID</label>
+        <input type="text" name="id" id="id" class="form-control">
+        
+        <!-- Image upload stuff -->
+        <label for="image">Images</label>
+        <input type="file" name="image" id="image" size="20">
+        
+        
+        <label for="post">Description</label>
+        <textarea name="post" class="editor">
+            {post}
+        </textarea>
     
-    <input name="Save" type="submit" value="Save" />
+        <input name="Save" type="submit" value="Save" />
+    </div>
 </form>
